57666738ca613c6fa6770c0d56f597025aade83e
ndambuki94 <ndambuki94@gmail.com>
2019-09-23 09:51:53 +0300

    add constructordetails

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,13 @@
 export class Quote {
-}
+
+    constructor(
+      public text: string,
+      public author: string,
+      public poster: string,
+      public posttime: Date,
+      public upvotes: number,
+      public downvotes: number
+    ) {  }
+  
+  }
+  
