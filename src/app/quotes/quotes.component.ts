89c612ed493b6d6ef2a496e46ddd0c9b3eae5c8b
ndambuki94 <ndambuki94@gmail.com>
2019-09-23 10:08:41 +0300

    add quotemodel

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  // It maintains quote Model
+  quoteModel: Quote;
+
+// It maintains quote form display status. By default it will be false.
+  showNew: Boolean = false;
+  
+// It will be either 'Save' or 'Update' based on operation.
+  submitType: string = 'Save';
 
   constructor() { }
 
